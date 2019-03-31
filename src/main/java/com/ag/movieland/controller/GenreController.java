7d05cf3fb0798aa0655421ee39ba723434560355
A.G <e_nazik@ukr.net>
2019-03-31 22:20:07 +0300

    removed extra spaces

--- src/main/java/com/ag/movieland/controller/GenreController.java
+++ src/main/java/com/ag/movieland/controller/GenreController.java
@@ -1,15 +1,15 @@
 package com.ag.movieland.controller;
 
-        import com.ag.movieland.entity.Genre;
-        import com.ag.movieland.service.IGenreService;
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.http.MediaType;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import com.ag.movieland.entity.Genre;
+import com.ag.movieland.service.IGenreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-        import java.util.List;
+import java.util.List;
 
 @RestController
 public class GenreController {
